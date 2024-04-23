e47d5e044b069ee134c2e05ea36400ef0c372ba9
evandroviero <evandro.viero@gmail.com>
2024-04-23 12:10:26 -0300

    desafio aula 02

--- AULA 02/desafio.sql
+++ AULA 02/desafio.sql
@@ -1,11 +1,26 @@
 -- 1. Obter todas as colunas das tabelas Clientes, Pedidos e Fornecedores
-
+SELECT * FROM customers;
+SELECT * FROM orders;
+SELECT * FROM suppliers;
 -- 2. Obter todos os Clientes em ordem alfabética por país e nome
-
+SELECT *
+FROM customers
+ORDER BY country, contact_name;
 -- 3. Obter todos os Pedidos por data
-
+SELECT * 
+FROM orders 
+ORDER BY order_date
+LIMIT 5;
 -- 4. Obter a contagem de todos os Pedidos feitos durante 1997
-
+SELECT COUNT(*) AS "Number of Orders During 1997"
+FROM orders
+WHERE order_date BETWEEN '1997-1-1' AND '1997-12-31';
 -- 5. Obter os nomes de todas as pessoas de contato onde a pessoa é um gerente, em ordem alfabética
-
+SELECT contact_name
+FROM customers
+WHERE contact_title LIKE '%Manager%'
+ORDER BY contact_name;
 -- 6. Obter todos os pedidos feitos em 19 de maio de 1997
+SELECT *
+FROM orders
+WHERE order_date = '1997-05-19';
